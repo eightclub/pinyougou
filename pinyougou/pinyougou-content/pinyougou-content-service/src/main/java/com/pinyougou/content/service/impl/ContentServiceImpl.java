87fd012ef4ef46a1e0e3a3c701c091f54197d46e
liaojianbin <meinitcasthavefun@foxmail.com>
2019-07-17 18:08:03 +0800

    门户系统大广告数据缓存加载实现

--- pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -9,6 +9,7 @@ import com.pinyougou.content.service.ContentService;
 import com.pinyougou.service.impl.BaseServiceImpl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -16,8 +17,13 @@ import java.util.List;
 @Service
 public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements ContentService {
 
+    //内容列表在redis中的key的名称
+    private static final String REDIS_CONTENT = "CONTENT_LIST";
     @Autowired
     private ContentMapper contentMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public PageInfo<TbContent> search(Integer pageNum, Integer pageSize, TbContent content) {
@@ -42,6 +48,16 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
     public List<TbContent> findContentListByCategoryId(Long categoryId) {
         List<TbContent> contentList = null;
 
+        try {
+            //从redis查询数据
+            contentList = (List<TbContent>) redisTemplate.boundHashOps(REDIS_CONTENT).get(categoryId);
+            if (contentList != null && contentList.size() > 0) {
+                return contentList;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         /**
          * --根据内容分类id查询其对应的所有有效的内容列表并且根据排序字段降序排序。
          * SELECT * FROM tb_content WHERE category_id = ? AND status = 1 ORDER BY sort_order DESC
@@ -55,6 +71,13 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
         example.orderBy("sortOrder").desc();
 
         contentList = contentMapper.selectByExample(example);
+
+        try {
+            //设置缓存
+            redisTemplate.boundHashOps(REDIS_CONTENT).put(categoryId, contentList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return contentList;
     }
