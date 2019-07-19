213c96f10eadbb103e464df1d03c511624e8abf6
liaojianbin <meinitcasthavefun@foxmail.com>
2019-07-19 16:06:47 +0800

    ElasticSearch-搜索

--- itcast-elasticsearch/src/test/java/cn/itcast/es/ElasticSearchTest.java
+++ itcast-elasticsearch/src/test/java/cn/itcast/es/ElasticSearchTest.java
@@ -2,11 +2,15 @@ package cn.itcast.es;
 
 import cn.itcast.es.dao.ItemDao;
 import com.pinyougou.pojo.TbItem;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -76,6 +80,55 @@ public class ElasticSearchTest {
     @Test
     public void delete(){
         itemDao.deleteById(100005945610L);
+    }
+
+    //通配符搜索
+    @Test
+    public void wildCardQuery(){
+
+        //创建查询构造对象
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+
+        /**
+         * 参数1：域名（与实体类中@Field配置的一致）
+         */
+        queryBuilder.withQuery(QueryBuilders.wildcardQuery("title", "手*"));
+
+        //获取查询对象
+        NativeSearchQuery query = queryBuilder.build();
+
+        //查询
+        AggregatedPage<TbItem> items = elasticsearchTemplate.queryForPage(query, TbItem.class);
+        System.out.println("总记录数：" + items.getTotalElements());
+        System.out.println("总页数：" + items.getTotalPages());
+        for (TbItem item : items) {
+            System.out.println(item);
+        }
+    }
+
+
+    //分词搜索
+    @Test
+    public void matchQuery(){
+
+        //创建查询构造对象
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+
+        /**
+         * 参数1：域名（与实体类中@Field配置的一致）
+         */
+        queryBuilder.withQuery(QueryBuilders.matchQuery("title", "极夜黑"));
+
+        //获取查询对象
+        NativeSearchQuery query = queryBuilder.build();
+
+        //查询
+        AggregatedPage<TbItem> items = elasticsearchTemplate.queryForPage(query, TbItem.class);
+        System.out.println("总记录数：" + items.getTotalElements());
+        System.out.println("总页数：" + items.getTotalPages());
+        for (TbItem item : items) {
+            System.out.println(item);
+        }
     }
 
 
