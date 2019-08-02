505dce420b3f8fdd3de1aff3a04c1d27c9c2b9f5
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-02 16:11:56 +0800

    秒杀首页商品数据缓存加载实现

--- pinyougou/pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/controller/SeckillOrderController.java
+++ pinyougou/pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/controller/SeckillOrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SeckillOrderController {
 
-    @Reference
+    @Reference(timeout = 10000)
     private SeckillOrderService seckillOrderService;
 
     /**
